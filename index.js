96c1c26a3dd778f5f94b0bae8b2b939b903754ab
Jan <janx.fetalvero@gmail.com>
2022-08-04 22:14:30 +0800

    grid size number function phase 1

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#gridContainer');
+const gridSizeBtn = document.querySelector('.gridNumber');
+
+function gridSize (){
+    gridSizeBtn.addEventListener('click', function(e){
+       let num= prompt('Enter a number of Grid')
+        console.log(num)
+    })
+    
+ 
+}
+gridSize()
+
+
+
+
+
+
 const createGrid = (size)=>{
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    container.style.gridTemplateRows = `repeat(${size}, auto)`;
     for (let i = 0; i < size * size; i++) {
         const grid = document.createElement('div');
         grid.classList.add('cell');
+        //grid.style.backgroundColor='red';
+        grid.onmouseover= function(){
+         grid.style.backgroundColor="red";
+         console.log('changecolor');
+        }
+        
         container.append(grid);
     }
 }
