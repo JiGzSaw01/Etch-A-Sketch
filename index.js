c8f119375092a0b03fe2c85e2187db2cc7d8498b
Jan <janx.fetalvero@gmail.com>
2022-08-08 20:32:39 +0800

    git size function phase 1.2

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const container = document.querySelector('#gridContainer');
 const gridSizeBtn = document.querySelector('.gridNumber');
-let defaultGridSize = 16
-function gridSize (){
-    gridSizeBtn.addEventListener('click', function(e){
-       let num= prompt('Enter a number of Grid')
-        
-    })
-    
- 
-}
-gridSize()
 
-
-
-
+let GridSize = 16
 
 const createGrid = (size)=>{
     
@@ -23,7 +11,7 @@ const createGrid = (size)=>{
         const grid = document.createElement('div');
         grid.classList.add('cell');
         //grid.style.backgroundColor='red';
-        grid.onmouseover= function(){
+        grid.onmousemove= function(){
          grid.style.backgroundColor="red";
          console.log('changecolor');
         }
@@ -31,5 +19,28 @@ const createGrid = (size)=>{
         container.append(grid);
     }
 }
-createGrid(defaultGridSize)
-console.log(createGrid)
+createGrid(1)
+//console.log(createGrid)
+
+const reset =()=>{
+    const cell = document.querySelectorAll('.cell')
+    cell.forEach(gridCell => gridCell.remove)
+   
+    
+}
+const gridSize=() =>{
+  gridSizeBtn.addEventListener('click', () =>{
+    let sizeBtn = prompt('Enter a number of Grid');
+    reset();
+    createGrid(sizeBtn)
+    
+    
+  })  
+  
+}
+gridSize()
+
+
+
+// make the reset function work
+//constrain when slecting the button size it is adding new cell insterad of changing the grid size
